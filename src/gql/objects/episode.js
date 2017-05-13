8d484a8e04da4c22d3672703f40012908a1e140d
Nicolas <nicolas.gasp@free.fr>
2017-05-13 23:37:58 +0200

    fix person

--- src/gql/objects/episode.js
+++ src/gql/objects/episode.js
@@ -10,8 +10,14 @@ module.exports = (resolvers, requireObject) => {
       id: { type: graphql.GraphQLID },
       air_date: { type: graphql.GraphQLString },
       episode_number: { type: graphql.GraphQLInt },
-      guest_stars: { type: new graphql.GraphQLList(Cast) },
-      crew: { type: new graphql.GraphQLList(Crew) },
+      guest_stars: { 
+        type: new graphql.GraphQLList(Cast),
+        resolve: resolvers.personList
+      },
+      crew: { 
+        type: new graphql.GraphQLList(Crew),
+        resolve: resolvers.personList
+      },
       name: { type: graphql.GraphQLString },
       overview: { type: graphql.GraphQLString },
       still_path: { type: graphql.GraphQLString },
